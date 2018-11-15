6e11956133b86f987b5861d3712d1fa38b87d0f5
Juan <koshtx>
2018-11-15 08:16:37 -0600

    chore: adding project

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+console.log(process.argv);
+
---
+++ notes.js
@@ -0,0 +1,5 @@
+console.log('Starting notes.js');
+
+module.exports.sumNumbers = (n1, n2) => {
+return n1 + n2;
+}
